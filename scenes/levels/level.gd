2643a127cd64e0e2b5bfadbbde0f0952f892eb35
alebul <master8423@gmail.com>
2023-07-18 06:59:47 +0300

    stop viewing on 1:53:22

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -10,11 +10,4 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-#	var pix: float = 1000
-#	var sec: float = 60
-#	var speed = (pix / sec) * delta
-#	$Logo.position.x += speed
-#	if $Logo.position.x > pix:
-#		$Logo.position.x = 0
-	
-#
+	pass
