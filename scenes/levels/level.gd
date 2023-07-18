19977e8b5f08ac6db91e527ccddef24157638378
alebul <master8423@gmail.com>
2023-07-18 16:32:10 +0300

    try to change laser direction

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -7,13 +7,15 @@ func _on_gate_player_entered_gate(body):
 	print("recive signal entered gate from gate ")
 	print(body)
 
-func _on_player_laser(pos):
-	var laser = laser_scene.instantiate()
+func _on_player_laser(pos, direction):
+	var laser = laser_scene.instantiate() as RigidBody2D
 	laser.position = pos
+	laser.linear_velocity = direction * laser.speed
 	$Projectiles.add_child(laser)
 
 
-func _on_player_grenade(pos):
-	var grenade = grenade_scene.instantiate()
+func _on_player_grenade(pos, direction):
+	var grenade = grenade_scene.instantiate() as RigidBody2D
 	grenade.position = pos
+	grenade.linear_velocity = direction * grenade.speed
 	$Projectiles.add_child(grenade)
--- scenes/levels/player.gd
+++ scenes/levels/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 signal laser(position)
-signal grenade(position)
+signal grenade(position, direction)
 
 var can_laser: bool = true
 var can_grenade: bool = true
@@ -16,17 +16,20 @@ func _process(_delta):
 	velocity = direction * speed
 	move_and_slide()
 	
+	# rotate
+	
+	var player_direction = (get_global_mouse_position() - position).normalized()
+	
 	# laser shooting input
 	if Input.is_action_pressed("primary action") and can_laser:
 		var laser_markers = $LaserStartPositions.get_children()
 		var selected_laser = laser_markers[randi() % laser_markers.size()]
-		laser.emit(selected_laser.global_position)
+		laser.emit(selected_laser.global_position, player_direction)
 		can_laser = false
 
 	if Input.is_action_pressed("secondary action") and can_grenade:
-		var laser_markers = $LaserStartPositions.get_children()
-		var selected_laser = laser_markers[randi() % laser_markers.size()]
-		grenade.emit(selected_laser.global_position)
+		var pos = $LaserStartPositions.get_children()[0].global_position	
+		grenade.emit(pos, player_direction)
 		can_grenade = false
 
 func _on_timer_laser_timeout():
