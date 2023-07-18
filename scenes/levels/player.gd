6e4e52a4d9a6326241b8b38a1dca60f6218239a4
alebul <master8423@gmail.com>
2023-07-18 10:44:48 +0300

    add timers for laser and grenade

--- scenes/levels/player.gd
+++ scenes/levels/player.gd
@@ -1,5 +1,8 @@
 extends CharacterBody2D
 
+var can_laser: bool = true
+var can_grenade: bool = true
+
 const speed: int = 300
 
 func _process(_delta):
@@ -11,8 +14,19 @@ func _process(_delta):
 	move_and_slide()
 	
 	# laser shooting input
-	if Input.is_action_pressed("primary action"):
+	if Input.is_action_pressed("primary action") and can_laser:
 		print("shoot laser")
+		can_laser = false
 		
-	if Input.is_action_pressed("secondary action"):
+	if Input.is_action_pressed("secondary action") and can_grenade:
 		print("shoot granade")
+		can_grenade = false
+
+func _on_timer_laser_timeout():
+	can_laser = true
+
+func _on_timer_grenade_timeout():
+	can_grenade = true
+
+
+
