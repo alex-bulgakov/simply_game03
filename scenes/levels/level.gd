e541945fe34ec7b3df02173d9e30d358f1cb8e5d
alebul <master8423@gmail.com>
2023-07-18 06:13:07 +0300

    add delta calculation

---
+++ scenes/levels/level.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+var pos: Vector2 = Vector2.ZERO
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var pix: float = 1000
+	var sec: float = 60
+	var speed = (pix / sec) * delta
+	$Logo.position.x += speed
+	if $Logo.position.x > pix:
+		$Logo.position.x = 0
+	
