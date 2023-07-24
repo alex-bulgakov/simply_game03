4d6c10d75ff7db88b00c436c14662e4b2837f454
alebul <master8423@gmail.com>
2023-07-24 17:06:49 +0300

    9:57:45

--- scenes/items/item.gd
+++ scenes/items/item.gd
@@ -4,6 +4,9 @@ var rotation_speed = 5
 var available_options = ['laser', 'laser', 'laser', 'grenade', 'health']
 var type = available_options[randi()%len(available_options)]
 
+var direction: Vector2
+var distance: int = randi_range(150, 250)
+
 func _ready():
 	if type == 'laser':
 		$Sprite2D.modulate = Color("8754d2")
@@ -11,6 +14,13 @@ func _ready():
 		$Sprite2D.modulate = Color("67781f")
 	if type == 'health':
 		$Sprite2D.modulate = Color("d0130a")
+		
+	# tween
+	var target_pos = position + direction * distance
+	var tween = create_tween()
+	tween.tween_property(self, "scale", Vector2(1,1), 0.3).from(Vector2(0,0))
+	tween.tween_property(self, "position", target_pos, 0.5)
+	
 	
 
 func _process(delta):
--- scenes/levels/item_container.gd
+++ scenes/levels/item_container.gd
@@ -2,5 +2,5 @@ extends StaticBody2D
 class_name ItemContainer
 
 signal open(pos, direction)
-
+var opened: bool = false
 @onready var current_direction: Vector2 = Vector2.DOWN.rotated(rotation)
